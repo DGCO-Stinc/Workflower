d610ab5c548386f8ebc9c34fc9b9be9af8d614f4
Rebitteration <36053415+Rebitteration@users.noreply.github.com>
2018-12-19 12:29:30 +0100

    Labs to 13 :fireworks:
    Nice.
    Die van mij gaan dus de vuilnisbak in :(

---
+++ Frontend/labo10.js
@@ -0,0 +1,30 @@
+function exchange(bedrag, valuta) {
+    if(valuta == "dollar/euro") {
+        bedrag = bedrag * euro_dollarkoers();        
+    } else if(valuta == "euro/dollar") {
+        bedrag = bedrag * dollar_eurokoers();
+    } else if(valuta == "roebel/euro") {
+        bedrag = bedrag * euro_roebelkoers();
+    } else {
+        bedrag = bedrag * roebel_eurokoers();
+    }
+    return(bedrag);
+}
+
+//Converteerders
+
+function euro_dollarkoers() {
+    return(1.36);
+}
+
+function dollar_eurokoers() {
+    return(0.74);
+}
+
+function euro_roebelkoers() {
+    return(48.40);
+}
+
+function roebel_eurokoers() {
+    return(0.02);
+}
